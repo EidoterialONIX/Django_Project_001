d1fb195ec290b2d7807dc56f8b19ac1a8162791d
Adrian Yurchyk <eidoterial.onix@gmail.com>
2024-10-07 13:50:20 +0300

    + home

--- projects/adrian_project/home/views.py
+++ projects/adrian_project/home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+
+def home(request):
+
+    content = """
+    
+        <h1>Home</h1>
+        <p>Constins</p>
+    
+    """
+
+    return HttpResponse(content)
